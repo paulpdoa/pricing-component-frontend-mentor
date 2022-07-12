0edf9a0592e70facba179644a347ef1c91471910
Paul Andres <polopdoandres@gmail.com>
2022-07-12 12:09:34 +0800

    first commit

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,14 @@
+
+const Toggle = () => {
+  return (
+    <div className="toggle">
+        <span className="toggle__subscription">Annually</span>
+        <div className="switch">
+            <div className="switch__circle__annual"></div>
+        </div>
+        <span className="toggle__subscription">Monthly</span>
+    </div>
+  )
+}
+
+export default Toggle
